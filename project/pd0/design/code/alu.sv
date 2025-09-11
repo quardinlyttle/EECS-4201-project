227947d7b3f570da1c90ffcc758270745723bc47
Quardin Lyttle <lyttleqd@gmail.com>
2025-09-11 16:16:09 -0400

    Completed ALU

--- project/pd0/design/code/alu.sv
+++ project/pd0/design/code/alu.sv
@@ -29,10 +29,31 @@ module alu #(
         output logic zero_o,
         output logic neg_o
     );
+   
+    //Case statement for alu opcodes
+    always_comb begin :opselect
+        case(sel_i):
+            ADD:        res_o = op1_i + op2_i;
+            SUB:        res_o = op1_i - op2_i;
+            AND:        res_o = op1_i & op2_i;
+            OR:         res_o = op1_i | op2_i;
+            default:    res_o = '0;
+        endcase
+    end
 
-    /*
-     * Process definitions to be filled by
-     * student below...
-     */
+    //Flags for negative and zero results. 
+    always_comb begin : flagassert
+        if(res_o == 'b0)begin
+            zero_o == 1'b1;
+        end
+        else if (res_o[DWIDTH-1]) begin
+            neg_o == 1'b1;
+        end
+        else begin
+            neg_o == '0;
+            zero_o == '0;
+        end
+
+    end
 
 endmodule: alu
