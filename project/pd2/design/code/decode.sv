1a56f6603d51c6757b1631ed6c3d4e7d1e3c3ae2
Quardin Lyttle <lyttleqd@gmail.com>
2025-10-05 23:45:48 -0400

    Incomplete decode Note: Decode needs to handle some aspects of Immediate genertion but also ensure it passes on all values Note: Imgen seems to be a dummy module? Will finish this Monday Morning.

--- project/pd2/design/code/decode.sv
+++ project/pd2/design/code/decode.sv
@@ -44,11 +44,105 @@ module decode #(
     output logic [2:0] funct3_o,
     output logic [4:0] shamt_o,
     output logic [DWIDTH-1:0] imm_o
-);	
+);
 
     /*
      * Process definitions to be filled by
      * student below...
      */
+
+    logic [6:0] opcode, funct7;
+    logic [4:0] rs1, rs2, rd, shiftamt;
+    logic [2:0] funct3;
+    logic [DWIDTH-1:0] instruction, imm_reg;
+    logic [AWIDTH-1:0] programcounter;
+
+    //Slice opcode out for further comibinational decode later
+    assign opcode = instruction[6:0];
+
+    //Assign wires for output
+    assign insn_o = instruction;
+    assign pc_o = programcounter;
+    assign opcode_o = opcode;
+    assign funct3_o = funct3;
+    assign funct7_o = funct7;
+    assign rs1_o = rs1;
+    assign rs2_o = rs2;
+    assign rd_o = rd;
+    assign imm_o = imm_reg;
+    assign shamt_o = shiftamt;
+
+    //Logic to accept new instructions and program counter as well as to reset.
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            instruction <='d0;
+            programcounter <='d0;
+        end
+        else begin
+            instruction <= insn_i;
+            programcounter <= pc_i;
+        end
+    end
+
+    //Decoding the opcodes
+    always_comb begin : Decode
+        case(opcode):
+
+            //R-Type instructions
+            7'b0110011: begin
+                //slice instruction
+                rd = instruction[11:7];
+                funct3 = instruction[14:12];
+                rs1 = instruction[19:15];
+                rs2 = instruction[24:20];
+                funct7 = instruction[31:25];
+            end
+
+            //I-Type Instructions:
+            7'0010011: begin
+                rd = instruction[11:7];
+                funct3 = instruction[14:12];
+                rs1 = instruction[19:15];
+                case(funct3):
+                    //Remember, all sign extended unless otherwise noted
+                    //ADDI, XORI, ORI, ANDI
+                    3'h0: imm_reg = {{DWIDTH-12{instruction[31]}}, instruction[31:20]};
+                    3'h4: imm_reg = {{DWIDTH-12{instruction[31]}}, instruction[31:20]};
+                    3'h6: imm_reg = {{DWIDTH-12{instruction[31]}}, instruction[31:20]};
+                    3'h7: imm_reg = {{DWIDTH-12{instruction[31]}}, instruction[31:20]};
+
+                    //Shift logical left
+                    /*Note, I am passing on the values of the immediate for the ALU or control
+                    to be able to establish the logical or airthmetic for right or left. 
+                    */
+                    3'h1: begin
+                        if(instruction[31:25]==0x00) begin
+                            shiftamt = instruction[11:7];
+                            imm_reg = {{DWIDTH-12{0}},instruction[31:25],instruction[11:7]};
+                        end
+                    end
+
+                    //Shift Right Logical and Shift Right Arithmetic
+                    3'h5: begin
+                        if(instruction[31:25]==0x00 ||instruction[31:25]==0x20) begin
+                            shiftamt = instruction[11:7];
+                            imm_reg = {{DWIDTH-12{0}},instruction[31:25],instruction[11:7]};
+                        end
+                    end
+
+                endcase
+
+            end
+            //Zero out everything on reset or unknown opcode
+            default : begin
+                rd = 'd0;
+                funct3 = 'd0;
+                rs1 = 'd0;
+                rs2 = 'd0;
+                funct7 = 'd0;
+
+            end
+        endcase
+    end
 
 endmodule : decode
