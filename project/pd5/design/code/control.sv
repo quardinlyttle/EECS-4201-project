10223b20827d332577a410169d7fff5a0d363e12
Kiet Le <ktlbusiness11@gmail.com>
2025-11-28 21:23:09 -0500

    PD5: fix memren in control

--- project/pd5/design/code/control.sv
+++ project/pd5/design/code/control.sv
@@ -179,7 +179,7 @@ module control #(
                 regwren_o = 1'b1;
                 rs1sel_o =  1'b0;
                 rs2sel_o =  1'b1;
-                memren_o =  1'b0;
+                memren_o =  1'b1;
                 memwren_o = 1'b0;
                 alusel_o = ADD;
             end
@@ -246,7 +246,7 @@ module control #(
                 regwren_o = 1'b1;
                 rs1sel_o =  1'b0;
                 rs2sel_o =  1'b0;
-                memren_o =  1'b1;
+                memren_o =  1'b0;
                 memwren_o = 1'b0;
                 alusel_o = ADD;
             end
